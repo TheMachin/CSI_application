61798344c12f665539a6013bb74c2d5387a5f65b
TheMachin <thibaultmartinec@gmail.com>
2016-04-05 20:25:58 +0200

    Revert "Revert "loihoin""
    This reverts commit ff0329e99024bcb65964f29089fbbbecd4f7b269.

--- vue/candidature.php
+++ vue/candidature.php
@@ -30,6 +30,7 @@ if(count($tabCanditure)>0)
                     <?php 
                     if($_SESSION["type"]!=="candidat")
                     {
+                        // truc
                         $dossier=$candidature->getDossier();
                         $candidat=$dossier->getCandidat();
                         ?>
@@ -48,7 +49,7 @@ if(count($tabCanditure)>0)
                     if($_SESSION["type"]!=="candidat")
                     {
                         ?>
-                    <td><a href="../Dossier/index.php?noDossier=<?php echo $dossier->getNo(); ?>">acceder</a></td>
+                    <td><a href="../Dossier/index.php?noDossier=<?php echo $dossier->getNo(); ?>">acceder</a></td> 
                         <?php
                     }
                     ?>
