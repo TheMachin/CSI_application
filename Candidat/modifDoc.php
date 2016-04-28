e9685468236cfbd1d18c5227c8b8ab53d0c05bb2
Juluto <julian.mayer94@gmail.com>
2016-04-29 00:25:45 +0200

    Ajout de modifDoc.php dans candidature
    Le candidat peut voir sa liste des documents ainsi que les modifier

--- Candidat/modifDoc.php
+++ Candidat/modifDoc.php
@@ -37,27 +37,26 @@ $tabCanditure=$candidatureSql->getCandidatureByUser($pdo, $candidat->getNom_cand
 <html>
     <head>
         <meta charset="UTF-8" />
-        <title>Confirmation candidature</title>
+        <title>Modification des documents</title>
     </head>
     <body>
-        <h2>Vous allez postuler dans cette formation :</h2>
-        <?php
-            $reponse = $pdo->query('SELECT u.NOM_UNIV, f.NOM_FORMATION, f.DOMAINE, f.NIVEAU, f.DATE_LIMITE, f.NBRE_PLACE_LIMITE, f.NO_FORMATION
-            FROM formation f, universite u
-            WHERE f.NO_UNIV = u.NO_UNIV
-            AND u.NOM_UNIV=\''. $_GET['ajout'] . '\'');
-            while ($donnees = $reponse->fetch())
-            {
-                echo '<p>Nom de l\'université : '. $donnees['NOM_UNIV'] .'</p>';
-                echo '<p>Nom de la formation : '. $donnees['NOM_FORMATION'] .'</p>';
-                echo '<p>Domaine : '. $donnees['DOMAINE'] . '</p>';
-                echo '<p>Niveau : '. $donnees['NIVEAU']. '</p>';
-                echo '<p>Date limite : '. $donnees['DATE_LIMITE']. '</p>';
-                echo '<p>Nombre de place limite : '. $donnees['NBRE_PLACE_LIMITE'].'</p>';
-                $noForm = $donnees['NO_FORMATION'];
-            }
-        ?>
-        <h3>Choisissez une lettre de motivation :</h3>
+        <h2>CV déposé :</h2>
+        <form action="index.php" method="post" >
+            <p>
+                <?php
+                    $curriculumVitae = $pdo->query('SELECT doc.NOM_DOC
+                    FROM dossier dos, document doc, contient_document cd
+                    WHERE doc.no_doc = cd.no_doc
+                    AND cd.no_dossier = dos.no_dossier
+                    AND doc.type_doc = "CV"
+                    AND dos.NOM_CANDIDAT =\''. $candidat->getNom_candidat().'\'');
+                    $CV = $curriculumVitae->fetch();
+                    echo '<input type="text" name="ldm" value="'. $CV['NOM_DOC'] . '" />';
+                ?>
+                <input type="submit" value="Modifier"/>
+            </p>
+        </form>
+        <h2>Liste des lettres de motivation :</h2>
         <?php
             $motivation = $pdo->query('SELECT doc.NOM_DOC
             FROM dossier dos, document doc, contient_document cd
@@ -65,6 +64,7 @@ $tabCanditure=$candidatureSql->getCandidatureByUser($pdo, $candidat->getNom_cand
             AND cd.no_dossier = dos.no_dossier
             AND doc.type_doc = "Lettre de motivation"
             AND dos.NOM_CANDIDAT =\''. $candidat->getNom_candidat().'\'');
+            echo $candidat->getNom_candidat();
         ?>
         <table>
             <tbody>
@@ -72,25 +72,28 @@ $tabCanditure=$candidatureSql->getCandidatureByUser($pdo, $candidat->getNom_cand
                     while ($lettre = $motivation->fetch()) {
                         echo '<tr>';
                         echo '<td>'. $lettre['NOM_DOC']. '</td>';
-                        echo '<td>
-                        <form action="index.php" method="post" >
-                        <input type="hidden" name="no_form" value="'. $noForm . '" />
-                        <input type="hidden" name="no_lettre" value="'. $lettre['NOM_DOC'] . '" />
-                        <input type="submit" value="Confirmer"/>
-                        </td>';
                         echo '</tr>';
                     }
                 ?>
             </tbody>
         </table>
-        <form action="index.php" method="post" >
-            <p>
-                <label>Nouvelle lettre de motivation :</label>
-                <input type="text" name="ldm" />
-                <?php
-                echo '<input type="hidden" name="no_form" value="'. $noForm . '" />'; ?>
-                <input type="submit" value="Confirmer"/>
-            </p>
-        </form>
+        <h2>Liste des justificatifs de diplôme</h2>
+            <form action="index.php" method="post" >
+                <p>
+                    <?php
+                        $justificatifDiplome = $pdo->query('SELECT doc.NOM_DOC
+                        FROM dossier dos, document doc, contient_document cd
+                        WHERE doc.no_doc = cd.no_doc
+                        AND cd.no_dossier = dos.no_dossier
+                        AND doc.type_doc = "Justificatif de diplôme"
+                        AND dos.NOM_CANDIDAT =\''. $candidat->getNom_candidat().'\'');
+                        while ($jD = $justificatifDiplome->fetch())
+                        {
+                            echo '<input type="text" name="jD" value="'. $jD['NOM_DOC'] . '" />';
+                            echo '<input type="submit" value="Modifier"/>';
+                        }
+                    ?>
+                </p>
+            </form>
     </body>
 </html>
