2678beb811979a244a1d3e36720866ee21720858
iphirus <lomre.david@gmail.com>
2016-04-28 17:28:23 +0200

    stats nb candidature

--- Gestionnaire/statistique.php
+++ Gestionnaire/statistique.php
@@ -1,8 +1,53 @@
 <?php
+include("../modele/connexion.php");
+include("../modele/CandidatSql.php");
+include("../modele/GestionnaireSql.php");
+include("../modele/DossierSql.php");
+include("../modele/PaysSql.php");
+include("../modele/CandidatureSql.php");
+include("../modele/FormationSql.php");
+include("../modele/ResponsableFSql.php");
+include("../modele/UniversiteSql.php");
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//recupere nom universite
+$univ = $pdo->query('select * from universite');
+//recupere nb candidature par universite
+$nbCand = $pdo->query('select count(NO_CANDIDATURE)as nb from universite, candidature where NO_FORMATION=NO_UNIV');
 
+
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Statistique</title>
+    </head>
+    <body>
+        <h1>
+            Statistique
+        </h1>
+        <TABLE BORDER="1"> 
+            <CAPTION> Candidatures par universite </CAPTION>
+            <TR>
+                <TH> Universite </TH>
+                <?php while($donneesUniv = $univ->fetch())
+                    echo '<th>' . $donneesUniv['NOM_UNIV'] . '</th>'; 
+                ?>
+            </TR> 
+            <TR> 
+                <TH> Nombre de candidature </TH> 
+                <?php while($donneesnbCand = $nbCand->fetch())
+                    echo '<td>' . $donneesnbCand['nb'] . '</td>';
+                ?>
+            </TR>
+        </TABLE>
+        <?php
+          
+        ?>
+    </body>
+</html>
